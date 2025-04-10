d22ecafad6968319d8672716257c684ad7d7cd82
jujcgu <juanjosecastrogutierrez@gmail.com>
2025-04-10 10:05:08 -0500

    UPDATE ApplicationConfig to include @Primary annotation for myThirdBean and REMOVE @Qualifier annotations from myFirstBean and mySecondBean; REFACTOR MyFirstService to use field injection for MyFirstClass

--- src/main/java/com/jujcgu/application/ApplicationConfig.java
+++ src/main/java/com/jujcgu/application/ApplicationConfig.java
@@ -1,22 +1,26 @@
 package com.jujcgu.application;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    @Qualifier("bean1")
     MyFirstClass myFirstBean() {
         return new MyFirstClass("First Bean");
     }
 
     @Bean
-    @Qualifier("bean2")
     MyFirstClass mySecondBean() {
         return new MyFirstClass("Second Bean");
+    }
+    
+    @Bean
+    @Primary
+    MyFirstClass myThirdBean() {
+        return new MyFirstClass("Third Bean");
     }
 
 }
--- src/main/java/com/jujcgu/application/MyFirstService.java
+++ src/main/java/com/jujcgu/application/MyFirstService.java
@@ -1,16 +1,13 @@
 package com.jujcgu.application;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
-	private final MyFirstClass myFirstClass;
-
-	public MyFirstService(@Qualifier("bean2") MyFirstClass myFirstClass) {
-		this.myFirstClass = myFirstClass;
-	}
+	@Autowired
+	private MyFirstClass myFirstClass;
 
 	public String tellAStrory() {
 		return "the dependency is saying: " + myFirstClass.sayHello();

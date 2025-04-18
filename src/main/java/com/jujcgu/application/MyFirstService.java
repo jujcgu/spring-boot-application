aed3307efefc80de9837f3edd6348eee4cccf6e2
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-04-17 22:42:26 -0500

    UPDATE MyFirstService to add support for additional properties from custom-file-2.properties

--- src/main/java/com/jujcgu/application/MyFirstService.java
+++ src/main/java/com/jujcgu/application/MyFirstService.java
@@ -3,10 +3,14 @@ package com.jujcgu.application;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Service;
 
 @Service
-@PropertySource("classpath:custom.properties")
+@PropertySources({
+		@PropertySource("classpath:custom.properties"),
+		@PropertySource("classpath:custom-file-2.properties")
+})
 public class MyFirstService {
 
 	private final MyFirstClass myFirstClass;
@@ -19,6 +23,9 @@ public class MyFirstService {
 
 	@Value("${my.custom.property.integer}")
 	private Integer customPropertyInteger;
+
+	@Value("${my.prop.2}")
+	private String customPropertyFromAnotherFile2;
 
 	public MyFirstService(@Qualifier("bean1") MyFirstClass myFirstClass) {
 		this.myFirstClass = myFirstClass;
@@ -38,6 +45,10 @@ public class MyFirstService {
 
 	public Integer getCustomPropertyInteger() {
 		return customPropertyInteger;
+	}
+
+	public String getCustomPropertyFromAnotherFile2() {
+		return customPropertyFromAnotherFile2;
 	}
 
 }
